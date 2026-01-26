fd022f25b2987a0a8474bbe6ebff57a21d80de79
Yago Marinho <marinho.rj2@hotmail.com>
2026-01-26 16:36:49 -0300

    with.required.keys header added

--- src/with.required.keys.ts
+++ src/with.required.keys.ts
@@ -1,3 +1,10 @@
+/*
+ * Copyright (c) 2025 Yago Marinho
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
 export type WithRequiredKeys<A extends {}, K extends keyof A> = Partial<
   Omit<A, K>
 > &
